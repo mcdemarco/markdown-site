f508661546b715d44bdd02f9c63ec3615473119d
M. C. DeMarco <mcd@mcdemarco.net>
2022-09-23 12:41:43 -0400

    Macified

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Ownership of these files doesn't seem to be important, but YMMV.
 SERVER_USERNAME=`whoami`
 USERNAME=`who am i | awk '{print $1}'`
 if [ "$SERVER_USERNAME" != 'root' ] || [ "$USERNAME" == 'root' ]; then
@@ -7,15 +8,17 @@ if [ "$SERVER_USERNAME" != 'root' ] || [ "$USERNAME" == 'root' ]; then
     exit 1;
 fi
 
-if [ ! -f /usr/bin/pandoc ]; then
+if [ ! -f /usr/local/bin/pandoc ]; then
     echo "Pandoc not found"
-    echo "Make sure Pandoc is installed to /usr/bin/pandoc and try again"
+    echo "Make sure Pandoc is installed to /usr/local/bin/pandoc and try again"
     exit 1;
 fi
 
-cp pandoc-template.html /usr/local/include/
-cp pandoc.conf /etc/apache2/conf-available/
-a2enmod mime
-a2enmod ext_filter
-a2enconf pandoc
+mkdir /usr/local/include/markdown-site/  #added the subdirectory just because.
+cp pandoc-template.html /usr/local/include/markdown-site/
+cp pandoc.conf /etc/apache2/other/  # These are loaded automatically in OSX. (See the end of httpd.conf.)
+#a2enmod mime # This should already be on.
+a2enmod ext_filter # In httpd.conf, just uncomment #LoadModule ext_filter_module libexec/apache2/mod_ext_filter.so
+#a2enconf pandoc  # Handled by cp pandoc.conf /etc/apache2/other/
 
+# Don't forget to sudo apachectl restart
